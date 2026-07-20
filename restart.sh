c8dda138e70d82a88baa03332991eb71fc254dcf
Heyang Liu <lhypds@gmail.com>
2026-07-21 03:33:39 +0900

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -5,5 +5,11 @@ ROOT="$(cd "$(dirname "$0")" && pwd)"
 
 echo "Restarting..."
 
+cd "$ROOT"
+echo "Pulling latest code..."
+git pull --ff-only
+
+"$ROOT/setup.sh"
+
 "$ROOT/stop.sh"
 "$ROOT/start.sh"
